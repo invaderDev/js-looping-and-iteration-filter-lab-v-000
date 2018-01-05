42de103a24a709f2f104a25578b3ccdba1fb81d3
Kris Thompson <kt.devcat@gmail.com>
2018-01-05 15:56:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, driverName) {
-  return array.filter(function (driver) {return string.toUpperCase() === driverName.toUpperCase()});
+  return drivers.filter(function (driver) {return string.toUpperCase() === driverName.toUpperCase()});
 }
 
 function matchName (drivers, name) {
